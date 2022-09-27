d36d5b30dac0569af5cf89edc982603c25d8baa7
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2022-09-26 22:03:48 -0300

    Adicionando Solução lógica da ToDo List

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,50 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    if (newTask.title == ''){
+      return alert('Não pode colocar valores vazios')
+    }
+    
+    setNewTaskTitle('')
+    setTasks(tasks => [...tasks, newTask])
+
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+
+    const taskToogled = tasks.map(task => task.id === id ? {
+      ...task, 
+    isComplete: !task.isComplete} : task )
+
+    setTasks(taskToogled)
+
+
+    /* ESSE FOI O MODO QUE EU TENTEI ANTES DE VER VIDEO AULA 
+   const taskComplete = tasks.filter(tarefa => tarefa.id === id)
+   const newTaskComplete = {
+    id: taskComplete[0].id,
+    title: taskComplete[0].title,
+    isComplete: !taskComplete[0].isComplete
+  }
+   setTasks(tasks => [...tasks.filter(tarefa => tarefa.id !== id), newTaskComplete])
+ */
+
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    setTasks(tasks => [...tasks.filter(tarefa => tarefa.id !== id)])
+
   }
 
   return (
@@ -33,6 +69,7 @@ export function TaskList() {
 
         <div className="input-group">
           <input 
+            className='firstInput'
             type="text" 
             placeholder="Adicionar novo todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
